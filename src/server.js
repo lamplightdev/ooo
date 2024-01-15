aa0063fd04c171cf00f637cac02cdd4498e1367b
Philip Stapelfeldt <philip.stapelfeldt@innoq.com>
2024-01-15 14:02:35 +0100

    make server runnable

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
+import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import { stream } from 'hono/streaming';
 import { render } from 'swtl';
 
-import { createReadableStreamFromAsyncGenerator } from './utils.js';
 import { template as listTemplate } from './templates/list.js';
+import { createReadableStreamFromAsyncGenerator } from './utils.js';
 
 const app = new Hono();
+const port = 3000;
 
 app.get('/', (ctx) => {
   return stream(ctx, async (stream) => {
@@ -14,6 +16,13 @@ app.get('/', (ctx) => {
       createReadableStreamFromAsyncGenerator(render(listTemplate()))
     );
   });
+});
+
+console.log(`Server is running on port ${port}`);
+
+serve({
+  fetch: app.fetch,
+  port,
 });
 
 export default app;
